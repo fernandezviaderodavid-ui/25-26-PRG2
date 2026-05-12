0741a99b8a9e10623413ee403cba0993137086b9
fernandezviaderodavid-ui <fernandezviaderodavid@gmail.com>
2026-05-12 09:30:49 +0200

    3. Control de turnos y flujo

--- entregas/fernandezdavid/Reto006/Reto006.java
+++ entregas/fernandezdavid/Reto006/Reto006.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Blackjack {
     static Scanner sc = new Scanner(System.in);
     static List<Integer> mazo = Arrays.asList(2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11);
-
+    
     public static void main(String[] args) {
         jugar();
     }
